d943853237f3c6f0cc37cfea634c79ca832bb327
hibana2077 <hibana2077@gmail.com>
2026-01-11 15:07:24 +1000

    feat: Add CAR scripts for CAPI training with varying parameters and update results table

---
+++ scripts/CAR/CAR003.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#PBS -P yp87
+#PBS -q gpuhopper
+#PBS -l ngpus=1
+#PBS -l ncpus=12
+#PBS -l mem=8GB
+#PBS -l walltime=08:00:00
+#PBS -l wd
+#PBS -l storage=scratch/yp87
+
+RUN_TAG="capi"
+
+module load cuda/12.6.2
+
+source /scratch/yp87/sl5952/CAPI/.venv/bin/activate
+export HF_HOME="/scratch/yp87/sl5952/CARROT/.cache"
+export HF_HUB_OFFLINE=1
+
+cd ../..
+python3 -u src/main.py \
+  --dataset stanford_cars --download --model resnet50 --pretrained \
+  --epochs 300 \
+  --lambda_lie 0.1 \
+  --seed 45 >> "CAR003.log" 2>&1
